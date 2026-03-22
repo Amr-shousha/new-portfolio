b667fa771fb35ff0fd0bde159ed1fcdfb4a1c218
Amr-shousha <amrshousha02@gmail.com>
2026-03-22 13:00:17 +0200

    background updates

--- assets/script.js
+++ assets/script.js
@@ -31,3 +31,69 @@ skillsSecondBtn.addEventListener("click", () => showSection(skillsSection));
 closeBtn.addEventListener("click", () => showSection(homeSection));
 
 showSection(homeSection);
+tsParticles.load({
+  id: "tsparticles",
+  options: {
+    fullScreen: { enable: false }, // MANDATORY: Keeps particles inside the div
+    background: { color: "transparent" },
+    // background: {
+    //   repeat: "no-repeat",
+    //   size: "10%",
+    //   position: "60% 50%",
+    // }
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: "repulse",
+        },
+        onHover: {
+          enable: true,
+          mode: "bubble",
+        },
+      },
+      modes: {
+        bubble: {
+          distance: 200,
+          duration: 2,
+          opacity: 0,
+          size: 0,
+          speed: 3,
+        },
+        repulse: {
+          distance: 400,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: { value: "#ffffff" },
+      move: {
+        direction: "none",
+        enable: true,
+        outModes: "out",
+        random: true,
+        speed: 0.3,
+      },
+      number: {
+        density: {
+          enable: true,
+        },
+        value: 600,
+      },
+      opacity: {
+        animation: {
+          enable: true,
+          speed: 5,
+        },
+        value: { min: 0.3, max: 0.6 },
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        value: 1,
+      },
+    },
+  },
+});
